7c97d1b420f8c6bf5781c24c079980d426ab00f8
bjiake <suyd@list.ru>
2023-10-13 23:39:29 +0700

    Рефактор

---
+++ backend/cmd/main.go
@@ -0,0 +1,26 @@
+// main.go
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin" // Используем фреймворк Gin для создания веб-сервиса
+)
+
+func main() {
+	router := gin.Default()
+
+	// Здесь вы можете определить маршруты и обработчики запросов
+
+	// Пример маршрута
+	router.GET("/api/banks", func(c *gin.Context) {
+		// Здесь можно обработать запрос и вернуть данные о банках
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	// Запускаем сервер на порту 8080
+	router.Run(":8080")
+}
